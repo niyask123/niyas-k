d5c09f0288edb80b1f881b092c445c69c5d8cd1f
niyaskknr <164878394+niyaskknr@users.noreply.github.com>
2024-06-22 17:31:37 +0530

    updations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,27 +8,27 @@ function Footer() {
       <div className="navbar bg-base-200 w-5/6 ">
         <footer className="footer flex flex-col items-center bg-black p-4 text-neutral-content">
           <aside className="items-center flex flex-col lg:grid-flow-col">
-            <img src="/public/img/icons/niyas1.png" className="rounded-full h-28" alt="" />
+            <img src="/img/icons/niyas1.png" className="rounded-full h-28" alt="" />
             <p>Copyright © 2024 - All right reserved</p>
           </aside>
           <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
             <a>
-              <img src="/public/img/svg/whatsapp.svg"
+              <img src="/img/svg/whatsapp.svg"
               className="object-contain h-8"
               alt="" />
             </a>
             <a>
-              <img src="/public/img/svg/insta.svg"
+              <img src="/img/svg/insta.svg"
               className="object-contain h-8"
               alt="" />
             </a>
             <a>
-              <img src="/public/img/svg/indeed.svg"
+              <img src="/img/svg/indeed.svg"
               className="object-contain h-8 bg-slate-400 rounded-full"
               alt="" />
             </a>
             <a>
-              <img src="/public/img/svg/git.svg"
+              <img src="/img/svg/git.svg"
               className="object-contain h-8 bg-slate-400 rounded-full"
               alt="" />
             </a>
